6b22dde353d0638b2d2743ea33cdff600acf0537
LAPTOP-Kenneth <kennethdammortensen@gmail.com>
2019-09-27 14:30:28 +0200

    Models and DB intializer

---
+++ ReadYourHeartOut/Data/DbInitializer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReadYourHeartOut.Models.Profiles;
+
+namespace ReadYourHeartOut.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(UserContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any users.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var users = new User[]
+            {
+                new User{UserName="Bob",Email="bob@dot.com",JoinDate=DateTime.Now},
+                new User{UserName="John",Email="john@dot.com",JoinDate=DateTime.Now},
+                new User{UserName="Jim",Email="Jim@dot.com",JoinDate=DateTime.Now},
+                new User{UserName="Gurlie",Email="gurlie@dot.com",JoinDate=DateTime.Now}
+            };
+
+            foreach (User user in users)
+            {
+                context.Users.Add(user);
+            }
+            context.SaveChanges();
+
+            var services = new Service[]
+            {
+                new Service{ServicesID=1000,ServiceName="Grammateket", Cost=29.99},
+                new Service{ServicesID=1001,ServiceName="MatematikLegFlex", Cost=49.99},
+                new Service{ServicesID=1002,ServiceName="IntoWords", Cost=99.99}
+            };
+
+            foreach (Service service in services)
+            {
+                context.Services.Add(service);
+            }
+            context.SaveChanges();
+
+
+        }
+    }
+}
---
+++ ReadYourHeartOut/Models/Profiles/Services.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReadYourHeartOut.Models.Profiles
+{
+    public class Service
+    {
+        public int ServicesID { get; set; }
+        public string ServiceName { get; set; }
+        public double Cost { get; set; }
+
+        public ICollection<User> Users { get; set; }
+    }
+}
