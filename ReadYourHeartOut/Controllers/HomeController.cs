62bc2a94e538a5f21dd3aba8a62fdaadafbf1799
Julian <juli11c0@edu.eal.dk>
2019-09-27 13:27:59 +0200

    copy HomeController

--- ReadYourHeartOut/Controllers/HomeController.cs
+++ ReadYourHeartOut/Controllers/HomeController.cs
@@ -5,13 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ReadYourHeartOut.Models;
+using Microsoft.Extensions.Logging;
 
 namespace ReadYourHeartOut.Controllers
 {
     public class HomeController : Controller
     {
+        //ILogger here...
+        private readonly ILogger logger;
+        public string Message;
+        //private int Count = 0;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
         public IActionResult Index()
         {
+            //Count++;
+            //ViewData["Count"] = Count + 1;
+            ViewData["Message"] = $"Welcome to our awesome Site, you visited the site at: {DateTime.Now.ToLongTimeString()}";
+            Message = $"Welcome to our awesome Site, you visited the site at: {DateTime.Now.ToLongTimeString()}";
+            logger.LogInformation("Message displayed: {Message}", Message);
             return View();
         }
 
